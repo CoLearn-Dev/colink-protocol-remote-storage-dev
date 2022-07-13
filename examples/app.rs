d3811f292c499e7d348bfdd0cdb5d344e3d1d8c0
stneng <git@stneng.com>
2022-07-13 16:41:13 +0000

    .

--- examples/app.rs
+++ examples/app.rs
@@ -1,4 +1,4 @@
-use colink_sdk_a::{decode_jwt_without_validation, CoLink, Participant};
+use colink_sdk_a::{decode_jwt_without_validation, CoLink, Participant, SubscriptionMessage};
 use prost::Message;
 use remote_storage_proto::*;
 use std::env;
@@ -28,8 +28,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         },
     ];
     let cl = CoLink::new(addr, jwt_a);
+    // create
     let params = CreateParams {
-        remote_key_name: "remote_storage_test".to_string(),
+        remote_key_name: "remote_storage_demo".to_string(),
         payload: msg.as_bytes().to_vec(),
         ..Default::default()
     };
@@ -41,10 +42,74 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let clb = CoLink::new(addr, jwt_b);
     let data = clb
         .read_or_wait(&format!(
-            "remote_storage:private:{}:remote_storage_test",
+            "remote_storage:private:{}:remote_storage_demo",
             user_id_a
         ))
         .await?;
     println!("{}", String::from_utf8_lossy(&data));
+
+    // read
+    let params = ReadParams {
+        remote_key_name: "remote_storage_demo".to_string(),
+        ..Default::default()
+    };
+    let mut payload = vec![];
+    params.encode(&mut payload).unwrap();
+    let task_id = cl
+        .run_task("remote_storage.read", &payload, &participants, true)
+        .await?;
+    let data = cl
+        .read_or_wait(&format!("tasks:{}:output", task_id))
+        .await?;
+    let path = String::from_utf8_lossy(&data);
+    let data = cl.read_or_wait(&path).await?;
+    println!("{}", String::from_utf8_lossy(&data));
+
+    // update
+    let queue_name = clb
+        .subscribe(
+            &format!("remote_storage:private:{}:remote_storage_demo", user_id_a),
+            None,
+        )
+        .await?;
+
+    let params = UpdateParams {
+        remote_key_name: "remote_storage_demo".to_string(),
+        payload: format!("update {}", msg).as_bytes().to_vec(),
+        ..Default::default()
+    };
+    let mut payload = vec![];
+    params.encode(&mut payload).unwrap();
+    cl.run_task("remote_storage.update", &payload, &participants, true)
+        .await?;
+
+    let mut subscriber = clb.new_subscriber(&queue_name).await?;
+    let data = subscriber.get_next().await?;
+    let message: SubscriptionMessage = prost::Message::decode(&*data).unwrap();
+    if message.change_type != "delete" {
+        println!("{}", String::from_utf8_lossy(&*message.payload));
+    } else {
+        Err("Receive delete change_type.")?
+    }
+
+    // delete
+    let params = DeleteParams {
+        remote_key_name: "remote_storage_demo".to_string(),
+        ..Default::default()
+    };
+    let mut payload = vec![];
+    params.encode(&mut payload).unwrap();
+    cl.run_task("remote_storage.delete", &payload, &participants, true)
+        .await?;
+
+    let data = subscriber.get_next().await?;
+    clb.unsubscribe(&queue_name).await?;
+    let message: SubscriptionMessage = prost::Message::decode(&*data).unwrap();
+    if message.change_type == "delete" {
+        println!("Deleted");
+    } else {
+        Err("Receive non-delete change_type.")?
+    }
+
     Ok(())
 }
